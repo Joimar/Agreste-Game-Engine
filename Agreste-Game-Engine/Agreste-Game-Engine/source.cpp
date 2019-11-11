b0db19420777692b7499314e7de2db6057c5a8e3
Joimar Brito <joimarbgf@gmail.com>
2019-11-11 17:40:23 -0300

    Tutorial - Projeção, Teste 1
    visualização estranha

--- Agreste-Game-Engine/Agreste-Game-Engine/source.cpp
+++ Agreste-Game-Engine/Agreste-Game-Engine/source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // GLEW
-//#define GLEW_STATIC
+
 #include <GL/glew.h>
 
 // GLFW
@@ -62,75 +62,155 @@ int main()
 	// Define the viewport dimensions
 	glViewport(0, 0, screenWidth, screenHeight);
 
+	// Setup OpenGL options
+	glEnable(GL_DEPTH_TEST);
+
 	// enable alpha support
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+
 	// Build and compile our shader program
 	Shader ourShader("core.vs", "core.fs");
 
+
 	// Set up vertex data (and buffer(s)) and attribute pointers
-	GLfloat vertices[] =
-	{
-		// Positions          // Colors           // Texture Coords
-		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+	// use with Orthographic Projection
+	/*
+	GLfloat vertices[] = {
+		-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
+		0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 0.0f,
+		0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+		0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+		-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,
+
+		-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+		0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
+		0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,
+		-0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 1.0f,
+		-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+
+		-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		-0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+		-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+		-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+
+		0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+		0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+		0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+
+		-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+		0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		-0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+		-0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+
+		-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f,
+		0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,
+		0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.0f,
+		-0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,
+		-0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 1.0f
 	};
-	GLuint indices[] =
-	{  // Note that we start from 0!
-		0, 1, 3, // First Triangle
-		1, 2, 3  // Second Triangle
+	 */
+
+	 // use with Perspective Projection
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-	GLuint VBO, VAO, EBO;
+
+
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
-	// Color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-	// Texture Coordinate attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(6 * sizeof(GLfloat)));
+	// TexCoord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0); // Unbind VAO
 
-						  // Load and create a texture
+
+	// Load and create a texture
 	GLuint texture;
-
-	int width, height;
-
-
-	// ===================
+	// ====================
 	// Texture
-	// ===================
+	// ====================
 	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
 	// Set our texture parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// Set texture filtering
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// Load, create texture and generate mipmaps
-	unsigned char *image = SOIL_load_image("images/som.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	int width, height;
+	unsigned char *image = SOIL_load_image("images/zebra.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+
+	glm::mat4 projection;
+	projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);
+	//projection =   glm::ortho(0.0f, ( GLfloat )screenWidth, 0.0f, ( GLfloat )screenHeight, 0.1f, 1000.0f);
 
 	// Game loop
 	while (!glfwWindowShouldClose(window))
@@ -141,38 +221,45 @@ int main()
 		// Render
 		// Clear the colorbuffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Draw the triangle
-		ourShader.Use();
 
-		// Create transformations
-		glm::mat4 transform(1);
-		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-		transform = glm::rotate(transform, (GLfloat)glfwGetTime() * (-5.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-
-		// Get matrix's uniform location and set matrix
-		GLint transformLocation = glGetUniformLocation(ourShader.Program, "transform");
-		glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
-
+		// Bind Textures using texture units
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
 
+		// Activate shader
+		ourShader.Use();
+
+		// Create transformations
+		glm::mat4 model;
+		glm::mat4 view;
+		model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f)); // use with perspective projection
+		//model = glm::rotate( model, 0.5f, glm::vec3( 1.0f, 0.0f, 0.0f ) ); // use to compare orthographic and perspective projection
+		//view = glm::translate( view, glm::vec3( screenWidth / 2, screenHeight / 2, -700.0f ) ); // use with orthographic projection
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); // use with perspective projection
+
+		// Get their uniform location
+		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+		GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
+		// Pass them to the shaders
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
 		// Draw container
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
-
 	// Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
 
